1bca00b375e9b31abf753ce18668ed385507d611
bcriger <bcriger@gmail.com>
2014-04-16 17:08:21 +0200

    Maybe I _will_ use this repo.

--- py-qcode/lattices.py
+++ py-qcode/lattices.py
@@ -1,6 +1,19 @@
-import networkx as nx
+"""
+Code Comparison Project, April 2014
+Ben Criger
+Shameless plagiarism from Bravyi/Haah
+"""
 
-class Point:
-	
+import networkx as nx
+from collections import namedtuple
+
+Point = namedtuple('Point', ['x','y','z']) 
+
+dist = lambda d, L: min(abs(d), L-abs(d)) #distance on closed 1d ring
 
 class SquareLattice:
+    """
+    This class represents a lattice of points with definite 
+    co-ordinates on which a stabilizer code can be defined.
+    """
+def __init__(self):
