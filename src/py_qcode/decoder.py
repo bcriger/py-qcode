308434a6077cdb1e8dc615a2f8954efd14cc1d87
bcriger <bcriger@gmail.com>
2014-06-10 15:12:42 -0400

    Began work on decoder class, minor refactor on lattice.py is required in order to update errors/syndromes once they have been set. This will require a little qecc import

--- src/py_qcode/decoder.py
+++ src/py_qcode/decoder.py
@@ -2,28 +2,39 @@ __all__ = ['Decoder', 'MWPMDecoder', 'RGBPDecoder', 'BHRGDecoder']
 
 class Decoder():
     """
-    docstring for Decoder
+    The role of a decoder is to infer an error given a syndrome. The 
+    overall pattern is  
     """
-    def __init__(self, arg):
-        self.arg = arg
+    def __init__(self, primal_lattice, dual_lattice, algorithm):
+        self.primal_lattice = primal_lattice
+        self.dual_lattice = dual_lattice
+        self.algorithm = algorithm
+
+    def infer(self):
+        """
+        Uses `self.algorithm` to update the error on the primal_lattice,
+        given the syndromes on the dual lattice.
+        """
+        #Pseudocode, requires refactor on lattice.py
+        self.primal_lattice.update(self.algorithm(self.dual_lattice),'error')
 
 class MWPMDecoder(Decoder):
     """
-    docstring for MWPMDecoder
+    Decoder based on minimum-weight perfect matching using the blossom algorithm. 
     """
     def __init__(self, arg):
         self.arg = arg
 
 class RGBPDecoder(Decoder):
     """
-    docstring for RGBPDecoder
+    A renormalization group / belief propagation decoder based on Duclos-Cianci and Poulin.
     """
     def __init__(self, arg):
         self.arg = arg
 
 class BHRGDecoder(Decoder):
     """
-    docstring for BHRGDecoder
+    Bravyi/Haah renormalization group decoder.
     """
     def __init__(self, arg):
         self.arg = arg
