2a0ef8b16afe0ee71a7e17b82610d65d97b2b89c
bcriger <bcriger@gmail.com>
2014-07-28 12:05:52 +0200

    put a little copypasta into utils.py

--- src/basic_decode.py
+++ src/basic_decode.py
@@ -2,29 +2,6 @@ import py_qcode as pq
 import networkx as nx
 from qecc import X, Z, I
 
-def error_print(lattice):
-    printnone = True
-    for point in test_lattice.points:
-        if not(point.error.op == 'I'):
-            print point
-            printnone = False
-
-    if printnone:
-        print "No Errors"
-
-    pass
-
-def syndrome_print(lattice):
-    printnone = True
-    for point in lattice.points:
-        if point.syndrome:
-            print point
-            printnone = False
-
-    if printnone:
-        print "No Syndromes"
-        pass
-
 #size=(4,4)
 #size=(40,40)
 size=(2,2)
@@ -47,19 +24,19 @@ test_logical_ops = pq.squoct_log_ops(test_lattice.total_size)
 
 test_model.act_on(test_lattice)
 print "Real Errors: \n============"
-error_print(test_lattice)
+pq.error_print(test_lattice)
 
 test_code.measure()
 print "Syndromes: \n=========="
-syndrome_print(test_dual_lattice)
+pq.syndrome_print(test_dual_lattice)
 
 test_decoder.infer()
 print "Error After Decoding:\n====================="
-error_print(test_lattice)
+pq.error_print(test_lattice)
 
 test_code.measure()
 print "Syndromes After Decoding:\n========================="
-syndrome_print(test_lattice)
+pq.syndrome_print(test_lattice)
 
 print "Logical Operators:\n=================="
 for op in test_logical_ops:
--- src/py_qcode/utils.py
+++ src/py_qcode/utils.py
@@ -9,7 +9,30 @@ from logical_operators import toric_log_ops
 
 import cPickle as pkl
 
-__all__ = ['sim_from_file', 'square_toric_code_sim']
+__all__ = ['sim_from_file', 'square_toric_code_sim', 'error_print', 'syndrome_print']
+
+def error_print(lattice):
+    printnone = True
+    for point in test_lattice.points:
+        if not(point.error.op == 'I'):
+            print point
+            printnone = False
+
+    if printnone:
+        print "No Errors"
+
+    pass
+
+def syndrome_print(lattice):
+    printnone = True
+    for point in lattice.points:
+        if point.syndrome:
+            print point
+            printnone = False
+
+    if printnone:
+        print "No Syndromes"
+        pass
 
 def sim_from_file(filename):
     """
@@ -56,6 +79,30 @@ def square_toric_code_sim(size, error_rate, n_trials, filename):
     sim_code = toric_code(sim_lattice, sim_dual_lattice)
     sim_decoder = mwpm_decoder(sim_lattice, sim_dual_lattice)
     sim_log_ops = toric_log_ops((size,size))
+
+    sim_keys = ['lattice', 'dual_lattice', 'error_model', 'code', 
+                            'decoder', 'logical_operators', 'n_trials']
+
+    sim_values = [sim_lattice, sim_dual_lattice, sim_model, sim_code, 
+                                sim_decoder, sim_log_ops, n_trials]
+    
+    sim_dict = dict(zip(sim_keys, sim_values))
+
+    sim = Simulation(**sim_dict)
+    sim.run()
+    sim.save(filename + '.sim')
+
+def squoct_sim(size, error_rate, n_trials, filename):
+    """
+    Copypasta from above, maybe I'll un-pasta this later. 
+    """
+    
+    sim_lattice = SquareOctagonLattice((size,size))
+    sim_dual_lattice = SquareOctagonLattice((size,size), is_dual=True)
+    sim_model = depolarizing_model(error_rate)
+    sim_code = square_octagon_code(sim_lattice, sim_dual_lattice)
+    sim_decoder = mwpm_decoder(sim_lattice, sim_dual_lattice)
+    sim_log_ops = squoct_log_ops(sim_lattice.size)
 
     sim_keys = ['lattice', 'dual_lattice', 'error_model', 'code', 
                             'decoder', 'logical_operators', 'n_trials']
