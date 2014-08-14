097d9d7f62488892d21acad0739cfbbe879d3b7b
bcriger <bcriger@gmail.com>
2014-08-14 14:18:59 +0200

    C version of squoct distance now works

--- src/c/squoct_dist.h
+++ src/c/squoct_dist.h
@@ -1,6 +1,9 @@
 #ifndef squoct_dist_h__
 #define squoct_dist_h__
 
-extern int dist(int int int int);
+typedef unsigned short int num_t;
+typedef enum { false, true } bool;
+
+extern num_t dist(num_t x1, num_t y1, num_t x2, num_t y2, num_t sz_x, num_t sz_y, char synd_type);
 
 #endif
--- src/c/squoct_dist_test.c
+++ src/c/squoct_dist_test.c
@@ -1,5 +1,33 @@
+#include "squoct_dist.h"
+#include "stdio.h"
+
 int main(int argc, char const *argv[])
 {
-	/* code */
-	return 0;
+    unsigned short int x1, y1, x2, y2, sz_x, sz_y;
+    num_t d;
+    char synd_type, quit_query;
+    while(1){
+        
+        puts("Enter x1"); scanf("%hu", &x1);
+        puts("Enter y1"); scanf("%hu", &y1);
+        puts("Enter x2"); scanf("%hu", &x2);
+        puts("Enter y2"); scanf("%hu", &y2);
+        puts("Enter sz_x"); scanf("%hu", &sz_x);
+        puts("Enter sz_y"); scanf("%hu", &sz_y);
+        puts("Enter synd_type"); scanf(" %c", &synd_type);
+
+        printf("First Point: (%d, %d)\n", x1, y1);
+        printf("Second Point: (%d, %d)\n", x2, y2);
+        printf("Syndrome Type: %c\n", synd_type);
+
+        d = dist((num_t)x1, (num_t)y1, (num_t)x2, (num_t)y2, 
+                    (num_t)sz_x, (num_t)sz_y, synd_type);
+        
+        printf("Resulting Distance: %hu\n", (unsigned short int)d);
+        
+        puts("Enter q to quit");
+        scanf(" %c", &quit_query);
+        if (quit_query=='q') break;
+    }
+    return 0;
 }
