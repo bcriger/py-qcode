e5cc08e9649258c57c3657121866d559be26084c
bcriger <bcriger@gmail.com>
2014-12-03 10:54:54 +0100

    Error models can now act on lists of qubits, untested

--- build/lib.linux-x86_64-2.7/py_qcode/test_lattice.py
+++ build/lib.linux-x86_64-2.7/py_qcode/test_lattice.py
@@ -1,10 +1,17 @@
 import lattice as lt
+import code as cd
+import utils as ut
+from qecc import I, X, Z
 
 #Global lattices to use in various tests
 g_sq_lat = lt.SquareLattice((8,8))
 g_d_sq_lat = lt.SquareLattice((8,8), is_dual=True)
 g_squoct_lat = lt.SquareOctagonLattice((4,4))
 g_uj_lat = lt.UnionJackLattice((4,4))
+
+#Codes to be measured as part of min_distance_path tests:
+toric_code = cd.toric_code(g_sq_lat, g_d_sq_lat)
+squoct_code = cd.square_octagon_code(g_squoct_lat, g_uj_lat)
 
 crds = lambda a: a.coords
 
@@ -95,3 +102,80 @@ def squ_squ_z_test():
 
 #__getitem__ tests, we confirm that, for all points in a lattice, 
 #lattice[(x, y)].coords == (x, y)
+
+def toric_items_test():
+    for pt in g_sq_lat.points:
+        assert g_sq_lat[pt.coords] == pt
+
+def dual_toric_items_test():
+    for pt in g_d_sq_lat.points:
+        assert g_d_sq_lat[pt.coords] == pt
+
+def squoct_items_test():
+    for pt in g_squoct_lat.points:
+            assert g_squoct_lat[pt.coords] == pt
+
+def uj_items_test():
+    for pt in g_uj_lat.points:
+        assert g_uj_lat[pt.coords] == pt
+
+#Path commutation/anticommutation tests: 
+#Check that min_distance_path produces Paulis which anticommute with 
+#stabilizers at their endpoints and not elsewhere:
+
+def path_com_pred(lat, d_lat, code, crd_lst, o_crd_lst, pauli):
+    """
+    Given a lattice, dual lattice, code, pair of coordinate lists, and
+    a pauli, returns true if laying down that Pauli on paths between 
+    every pair of points across the lists results in syndromes of the 
+    matching type on those points, and no syndromes elsewhere.
+    """
+    #Put the identity down on the lattice
+    for prml_pt in lat.points:
+        prml_pt.error = I
+
+    for crd in crd_lst:
+        for o_crd in o_crd_lst:
+            path = lat.min_distance_path(crd, o_crd, pauli.op)
+            if len(path) > 0:
+                for coord in path:
+                    lat[coord].error *= pauli
+                code.measure()
+                if d_lat[crd].syndrome != pauli.op:
+                    return False
+                if d_lat[o_crd].syndrome != pauli.op:
+                    return False
+                d_lat[crd].syndrome = None
+                d_lat[o_crd].syndrome = None
+                for dual_pt in d_lat.points:
+                    if dual_pt.syndrome:
+                        return False
+            for coord in path:
+                lat[coord].error = I
+
+    #If False hasn't yet been returned, return True
+    return True
+
+def toric_path_code_test():
+    for pauli, crd_lst in zip([X, Z], [lt._odd_odds(*g_sq_lat.size),
+                                    lt._even_evens(*g_sq_lat.size)]):
+        
+        assert path_com_pred(g_sq_lat, g_d_sq_lat, toric_code, 
+                        crd_lst, crd_lst, pauli)
+    return None
+    
+def squoct_path_code_test():
+    x_oct_lst = lt._squoct_affine_map(lt._even_evens(*g_squoct_lat.size))
+    z_oct_lst = lt._squoct_affine_map(lt._odd_odds(*g_squoct_lat.size))
+    sq_lst = lt._squoct_affine_map(lt.skew_coords(*g_squoct_lat.size))
+    crd_1 = [sq_lst, x_oct_lst, z_oct_lst, sq_lst, 
+                    sq_lst, x_oct_lst, z_oct_lst, sq_lst]
+    crd_2 = [sq_lst, sq_lst, sq_lst, sq_lst, x_oct_lst,
+                    x_oct_lst, z_oct_lst, z_oct_lst]
+    p_lst = [X, Z, X, Z, Z, Z, X, X]
+    count = 0
+    for crd_lst, o_crd_lst, pauli in zip(crd_1, crd_2, p_lst):
+        print count
+        count += 1
+        assert path_com_pred(g_squoct_lat, g_uj_lat, squoct_code, 
+                                        crd_lst, o_crd_lst, pauli)
