29c02c87676119b1b4f591ebb4c478e83dfe3cfe
Ben Criger <bcriger@gmail.com>
2015-01-14 11:34:23 +0100

    Attempting cluster-side commit

---
+++ build/lib/py_qcode/c_lib_test.py
@@ -0,0 +1,37 @@
+"""
+This script will call the function 'dist' from libqcode_dist.so in
+../c, using ctypes.
+"""
+from ctypes import CDLL, c_ushort, c_char
+
+libqcode_dist = CDLL('../c/libqcode_dist.so')
+
+total_size = (24, 24)
+
+#Convenience function for checking distances:
+def print_dist(pt1, pt2):
+    for synd_type in 'XZ':
+        x1, y1 = map(c_ushort, pt1); x2, y2 = map(c_ushort, pt2)
+        sz_x, sz_y = map(c_ushort, total_size)
+        delta = libqcode_dist.squoct_dist(x1, y1, x2, y2, sz_x, sz_y, 
+                                                    c_char(synd_type))
+        print "{3}-type distance between {0} and {1}: {2}".format(
+            pt1, pt2, delta, synd_type)
+    pass
+
+"""
+Initialize a set of test point pairs, each pair tests a different 
+aspect of the function.
+"""
+
+#2D Octagon-Octagon wrapping in both directions:
+pt1 = (4, 4); pt2 = (22, 22)
+print_dist(pt1, pt2)
+
+#2D Square-Octagon wrapping in both directions:
+pt1 = (7, 4); pt2 = (22, 22)
+print_dist(pt1, pt2)
+
+#2D Square-Square wrapping in both directions:
+pt1 = (7, 4); pt2 = (7, 13)
+print_dist(pt1, pt2)
---
+++ build/lib/py_qcode/logical_operators.py
@@ -0,0 +1,84 @@
+from qecc import Pauli, com
+from lattice import _evens, _odds
+
+__all__ = ['LogicalOperator', 'toric_log_ops', 'squoct_log_ops']
+
+class LogicalOperator():
+    """
+    This class wraps a function which tests anticommutation on a 
+    lattice.
+    """
+    def __init__(self, name, pauli, coord_list):
+        self.name = name
+        self.pauli = pauli
+        self.coord_list = coord_list
+
+    def __repr__(self):
+        return 'Operator {0}: {1} on coordinates {2}'.format(
+            self.name, self.pauli, self.coord_list)
+    
+    def test(self, lattice):
+        test_pauli = reduce(lambda a, b: a.tens(b), 
+                        [lattice[coord].error for coord in self.coord_list])
+        return self.name, com(test_pauli, self.pauli)
+
+def toric_log_ops(sz_tpl):
+    """
+    This is really poor design, but I'm kind of tired of this code. 
+    Maybe that's causative. Anyway, this returns a list of 
+    LogicalOperator objects that matches the toric code defined on a 
+    rectangular lattice.
+    """
+    if len(sz_tpl) != 2:
+        raise ValueError('Only 2D codes are supported in this'+\
+         'function for now, maybe define your operators manually')
+    x_1 = LogicalOperator("X_1", Pauli('X' * sz_tpl[0]),
+                            [(x, 1) for x in _evens(sz_tpl[0])])
+    x_2 = LogicalOperator("X_2", Pauli('X' * sz_tpl[1]),
+                            [(1, x) for x in _evens(sz_tpl[1])])
+    z_1 = LogicalOperator("Z_1", Pauli('Z' * sz_tpl[1]),
+                            [(0, x) for x in _odds(sz_tpl[1])])
+    z_2 = LogicalOperator("Z_2", Pauli('Z' * sz_tpl[0]),
+                            [(x, 0) for x in _odds(sz_tpl[0])])
+    
+    return [x_1, x_2, z_1, z_2]
+
+def squoct_log_ops(total_size):
+    """
+    Returns a list of LogicalOperator objects matching the non-gauge
+    qubits from a concatenated [[4,2,2]]/toric code.
+    """
+    if len(total_size) != 2:
+        raise ValueError('Only 2D codes are supported in this'+\
+         'function for now, maybe define your operators manually')
+    
+    edge_0 = sorted(range(0, total_size[0], 6) + range(2, total_size[0], 6))
+    edge_1 = sorted(range(0, total_size[1], 6) + range(2, total_size[1], 6))
+    shift_edge_0 = sorted(range(3, total_size[0], 6) + range(5, total_size[0], 6))
+    shift_edge_1 = sorted(range(3, total_size[1], 6) + range(5, total_size[1], 6))
+    
+    #Tough to tell what is/is not a gauge operator. Alternate sets 
+    #defined below:
+    '''
+    x_1 = LogicalOperator("X_1", Pauli('X' * (total_size[0] / 3)),
+                            [(x, 0) for x in shift_edge_0])
+    x_2 = LogicalOperator("X_2", Pauli('X' * (total_size[1] / 3)),
+                            [(0, x) for x in shift_edge_1])
+    z_1 = LogicalOperator("Z_1", Pauli('Z' * (total_size[1] / 3)),
+                            [(3, x) for x in edge_1])
+    z_2 = LogicalOperator("Z_2", Pauli('Z' * (total_size[0] / 3)),
+                            [(x, 3) for x in edge_0])
+    
+    '''
+    x_1 = LogicalOperator("X_1", Pauli('X' * (total_size[1] / 3)),
+                            [(3, x) for x in edge_1])
+    x_2 = LogicalOperator("X_2", Pauli('X' * (total_size[0] / 3)),
+                            [(x, 3) for x in edge_0])
+    z_1 = LogicalOperator("Z_1", Pauli('Z' * (total_size[0] / 3)),
+                            [(x, 0) for x in shift_edge_0])
+    z_2 = LogicalOperator("Z_2", Pauli('Z' * (total_size[1] / 3)),
+                            [(0, x) for x in shift_edge_1])
+    
+
+
+    return [x_1, x_2, z_1, z_2]
